d645631cf5ea13455fac00be492032df59b9c8cb
atleSkaanes <atle.skaanes@gmail.com>
2026-02-25 14:01:32 +0100

    Added -o funcionality And made IO errors into printable errors, instead of crashing the program with a panic

--- src/bin/cfgfuzz.rs
+++ src/bin/cfgfuzz.rs
@@ -1,4 +1,7 @@
-use std::{fs::OpenOptions, io::Read};
+use std::{
+    fs::OpenOptions,
+    io::{Read, Write},
+};
 
 use cfg_fuzzer::{cliargs, generate::generate_code, parser::parse};
 use clap::Parser;
@@ -9,7 +12,16 @@ const CLEAR: &str = "\x1b[0m";
 fn main() -> std::io::Result<()> {
     let args = cliargs::Args::parse();
 
-    let src = open_file(args.grammar.clone())?;
+    let src = match open_file(&args.grammar) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!(
+                "{}err{}: While opening '{}': {}",
+                RED, CLEAR, args.grammar, e
+            );
+            std::process::exit(1);
+        }
+    };
 
     let ast = match parse(&src, &args.grammar, &args.terms.into_boxed_slice()) {
         Ok(ast) => ast,
@@ -22,19 +34,38 @@ fn main() -> std::io::Result<()> {
     let generated = match generate_code(ast, &args.start, &mut rand::rng()) {
         Ok(g) => g,
         Err(e) => {
-            eprintln!("{}err{}: {}", RED, CLEAR, e);
+            eprintln!("{}io err{}: {}", RED, CLEAR, e);
             std::process::exit(1);
         }
     };
 
-    println!("{}", generated);
+    if let Some(out) = args.outfile {
+        if let Err(e) = write_file(&out, &generated) {
+            eprintln!("{}err{}: While writing to '{}': {}", RED, CLEAR, out, e);
+            std::process::exit(1);
+        }
+    } else {
+        println!("{}", generated);
+    }
 
     Ok(())
 }
 
-fn open_file(path: String) -> std::io::Result<String> {
+fn open_file(path: &str) -> std::io::Result<Box<str>> {
     let mut file = OpenOptions::new().read(true).open(path)?;
     let mut str = String::new();
     let _ = file.read_to_string(&mut str);
-    Ok(str)
+    Ok(str.into())
+}
+
+fn write_file(path: &str, contents: &str) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(path)?;
+
+    _ = file.write(contents.as_bytes())?;
+
+    Ok(())
 }
